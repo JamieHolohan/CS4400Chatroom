e347e755ebdef159902043b447e0880c7d6784cc
Jamie Holohan <holohaj@tcd.ie>
2017-11-14 16:05:41 +0000

    Scripts and JAR

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+gnome-terminal -e "java -jar ChatServer.jar $1"
+gnome-terminal -e "telnet localhost 4242"
+gnome-terminal -e "telnet localhost 4242"
+gnome-terminal -e "telnet localhost 4242"
